3306e63541e4e9ffef1422651fba12770feda96c
cbosmans <cederic.bosmans@businessdecision.be>
2021-04-22 15:37:09 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# custom_tetris_RL
---
+++ basic_agent.py
@@ -0,0 +1,34 @@
+import random
+
+from time import sleep 
+from engine import TetrisEngine
+
+class BasicAgent():
+    def __init__(self):
+        # Initializes a Tetris playing field of width 10 and height 20.
+        self.env = TetrisEngine()
+
+    def run(self):
+        # Loop to keep playing games
+        while True:
+            # Variable to indicate whether the game has ended or not
+            done = False
+            # Resets the environment
+            state = self.env.reset()
+            
+            # Loop that keeps making moves as long as the game hasn't ended yet
+            while not done:
+                # Picks a random action
+                action = random.randint(0,5)
+                action = 5
+                # Performs the action in the game engine
+                next_state, reward, done, info = self.env.step(action)
+                # Render the game state
+                self.env.render()
+                # Sleep to make sure a human can follow the gameplay (if you're not rendering, this isn't neccessary of course)
+                sleep(0.05)
+                    
+                    
+if __name__ == '__main__':
+    agent = BasicAgent()
+    agent.run()
